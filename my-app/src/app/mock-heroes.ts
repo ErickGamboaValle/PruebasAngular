09c2529f4e8ab9ee043464ac6587a2fbf80dd884
Erick <is710115@iteso.mx>
2023-02-05 16:33:22 -0600

    Initial commit

---
+++ my-app/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 0, name: 'Halv Fordrojd' },
+    { id: 12, name: 'Dr. Mundo' },
+    { id: 13, name: 'Kaisa' },
+    { id: 14, name: 'Hecarim' },
+    { id: 15, name: 'Jhin' },
+    { id: 16, name: 'Fiddlesticks' },
+    { id: 17, name: 'Darius' },
+    { id: 18, name: 'Master yi' },
+    { id: 19, name: 'Vi' },
+    { id: 20, name: 'Graves' }
+];
