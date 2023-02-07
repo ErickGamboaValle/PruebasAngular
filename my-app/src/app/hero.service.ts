b5edd9412b03f4ba6356594f2c8653df0395f7e5
Erick <is710115@iteso.mx>
2023-02-06 22:10:54 -0600

    Adding Messages to the project

--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -1,9 +1,20 @@
+import { Hero } from './hero'
+import { HEROES } from './mock-heroes'
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('Fetched Heroes');
+    return heroes;
+  }
 }
+
